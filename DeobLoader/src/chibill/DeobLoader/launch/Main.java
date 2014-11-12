4b9898cd7ed20fe5c404f8340452b6e0deefad88
bill <chiwill@me.com>
2014-11-11 18:49:22 -0600

    update have basic class loader set up

--- DeobLoader/src/chibill/DeobLoader/launch/Main.java
+++ DeobLoader/src/chibill/DeobLoader/launch/Main.java
@@ -1,12 +1,14 @@
 package chibill.DeobLoader.launch;
 
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		
+		
 	}
+	
+	
 
 }
--- DeobLoader/src/chibill/DeobLoader/loader/Load.java
+++ DeobLoader/src/chibill/DeobLoader/loader/Load.java
@@ -1,9 +1,37 @@
 package chibill.DeobLoader.loader;
 
+import java.net.URLClassLoader;
+
+import net.minecraft.launchwrapper.LaunchClassLoader;
+
 public class Load {
 
-	public void GetChanges(){
-		
+	private LaunchClassLoader classLoader;
+
+	public void SetupClassLoader(){
+
+		classLoader = new LaunchClassLoader(((URLClassLoader)getClass().getClassLoader()).getURLs());
+	
+		classLoader.registerTransformer("cpw.mods.fml.common.asm.transformers.DeobfuscationTransformer");
+	
+	}
+
+public void Start(String main){
+	
+	try {
+		final Class<?> clazz = Class.forName(main, false, classLoader);
+	} catch (ClassNotFoundException e) {
+
+				
 	}
 	
+	
+}
+
+	public void AddTargetToPath(String target){
+
+
+
+	}
+
 }
