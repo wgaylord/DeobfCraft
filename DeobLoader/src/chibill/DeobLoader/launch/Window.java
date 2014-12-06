a0a1ce5303319509ff7f904907bd656c0de7e4ab
bill <chiwill@me.com>
2014-12-05 18:19:42 -0600

    Made it load the files right.
    Can load all classes and start the traget program not sure if it
    actually deobfuscates yet,

--- DeobLoader/src/chibill/DeobLoader/launch/Window.java
+++ DeobLoader/src/chibill/DeobLoader/launch/Window.java
@@ -1,20 +1,37 @@
 package chibill.DeobLoader.launch;
-
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import chibill.DeobLoader.loader.Load;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.lang.reflect.InvocationTargetException;
 
 public class Window extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7383677203665318776L;
+	
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private static Load load = new Load();
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		load.SetupClassLoader();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -32,11 +49,54 @@ public class Window extends JFrame {
 	 */
 	public Window() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 369, 328);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Launch");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			
+				load.AddTargetToPath(textField_1.getText());
+				load.AddTargetToPath(textField_2.getText());
+				
+				try {
+					load.Start(textField.getText());
+				} catch (NoSuchMethodException | SecurityException
+						| IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		btnNewButton.setBounds(70, 221, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Enter the full class name of the file to launch.");
+		lblNewLabel.setBounds(10, 34, 225, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(43, 59, 156, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblAndAlsoIts = new JLabel("And also its jar name.");
+		lblAndAlsoIts.setBounds(70, 84, 104, 14);
+		contentPane.add(lblAndAlsoIts);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(43, 158, 156, 20);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(43, 109, 156, 20);
+		contentPane.add(textField_2);
 	}
-
 }
--- DeobLoader/src/chibill/DeobLoader/loader/Load.java
+++ DeobLoader/src/chibill/DeobLoader/loader/Load.java
@@ -1,28 +1,46 @@
 package chibill.DeobLoader.loader;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URLClassLoader;
+
+import javax.swing.JOptionPane;
 
 import net.minecraft.launchwrapper.LaunchClassLoader;
 
 public class Load {
 
-	private LaunchClassLoader classLoader;
+	private static LaunchClassLoader classLoader;
 
-	public void SetupClassLoader(){
+	public  void SetupClassLoader(){
 
 		classLoader = new LaunchClassLoader(((URLClassLoader)getClass().getClassLoader()).getURLs());
 	
-		classLoader.registerTransformer("cpw.mods.fml.common.asm.transformers.DeobfuscationTransformer");
-	
+		classLoader.registerTransformer("chibill.DeobLoader.loader.tweaker");
+		
+		
+        
+        classLoader.addTransformerExclusion("cpw.mods.fml.repackage.");
+        classLoader.addTransformerExclusion("cpw.mods.fml.relauncher.");
+        classLoader.addTransformerExclusion("cpw.mods.fml.common.asm.transformers.");	
+
+        classLoader.addTransformerExclusion("chibill.DeobLoader.loader.");
 	}
 
-public void Start(String main){
-	
+public void Start(String main) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+	System.out.println(main);
 	try {
 		final Class<?> clazz = Class.forName(main, false, classLoader);
-	} catch (ClassNotFoundException e) {
+		final Method mainMethod = clazz.getMethod("main", new Class[]{String[].class});
 
-				
+        mainMethod.invoke(null, (Object)new String[0]);
+				} catch (ClassNotFoundException e) {
+
+		JOptionPane.showMessageDialog(null, "ERROR! ");
+		e.printStackTrace();		
+		
 	}
 	
 	
@@ -30,7 +48,17 @@ public void Start(String main){
 
 	public void AddTargetToPath(String target){
 
-
+		if(target.isEmpty()){
+			target = "";
+		}
+		
+		try {
+			classLoader.addURL(new File(target).toURI().toURL());
+		} catch (MalformedURLException e) {
+			
+			e.printStackTrace();
+			
+					}
 
 	}
 
